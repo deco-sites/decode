e2bbff416ca10ceb787652c64ec208e36e5f330f
viktormarinho <viktormpcs@gmail.com>
2023-05-21 15:54:33 -0300

    autolint

--- sections/LessonNavbar.tsx
+++ sections/LessonNavbar.tsx
@@ -1,13 +1,12 @@
-
 export interface Props {
-    author: string;
+  author: string;
 }
 
 export default function LessonNavbar({ author }: Props) {
-    return (
-        <div class="p-4 border-b border-white mb-16 flex justify-between">
-            <a href="/" class="underline text-white">Back to Homepage</a>
-            <p class="text-white font-semibold">{author}</p>
-        </div>
-    )
+  return (
+    <div class="p-4 border-b border-white mb-16 flex justify-between">
+      <a href="/" class="underline text-white">Back to Homepage</a>
+      <p class="text-white font-semibold">{author}</p>
+    </div>
+  );
 }
