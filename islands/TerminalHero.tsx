f3af400398e44f8b05715c51bb6e80d3ce6605a6
viktormarinho <viktormpcs@gmail.com>
2023-05-21 11:52:45 -0300

    autolint

--- islands/TerminalHero.tsx
+++ islands/TerminalHero.tsx
@@ -1,17 +1,17 @@
 import { useJs } from "$store/sdk/useJs.tsx";
 
 export interface Props {
-    title: string;
-    code?: {
-        expectedOutput: string;
-        lines: string[];
-    };
-    lesson?: {
-      lessonTitle: string;
-      lessonSections: {
-        text: string
-      }[];
-    };
+  title: string;
+  code?: {
+    expectedOutput: string;
+    lines: string[];
+  };
+  lesson?: {
+    lessonTitle: string;
+    lessonSections: {
+      text: string;
+    }[];
+  };
 }
 
 export default function Terminal({ title, code, lesson }: Props) {
@@ -29,32 +29,41 @@ export default function Terminal({ title, code, lesson }: Props) {
       </h2>
       <div class="sm:w-2/3 w-4/5 flex flex-col sm:flex-row gap-8 justify-around">
         <div class="flex flex-col items-start gap-4 sm:w-1/2">
-          <h3 class="text-xl font-semibold">{lesson ? lesson.lessonTitle : "Javascript Hello world"}</h3>
-          {lesson ? (
-            lesson.lessonSections.map(section => ((
-              <p class="text-lg">{section.text}</p>
-            )))
-          ): (
-            <>
-            <p class="text-lg">Javascript intro - Part 1: Declaring variables</p>
-            <p class="text-lg">
-              To declare variables in javascript, you can use the{" "}
-              <span class="text-amber-500">let</span> keyword.
-            </p>
-            </>
-          )}
-          <div class="mockup-code bg-primary w-full">
-            {code ? (
-              code.lines.map(line => <pre><code>{line}</code></pre>)
-            ): (
+          <h3 class="text-xl font-semibold">
+            {lesson ? lesson.lessonTitle : "Javascript Hello world"}
+          </h3>
+          {lesson
+            ? (
+              lesson.lessonSections.map(
+                (section) => <p class="text-lg">{section.text}</p>,
+              )
+            )
+            : (
               <>
-                <pre><code>let name = "John Doe";</code></pre>
-                <pre><code>console.log(name);</code></pre>
+                <p class="text-lg">
+                  Javascript intro - Part 1: Declaring variables
+                </p>
+                <p class="text-lg">
+                  To declare variables in javascript, you can use the{" "}
+                  <span class="text-amber-500">let</span> keyword.
+                </p>
               </>
             )}
+          <div class="mockup-code bg-primary w-full">
+            {code
+              ? (
+                code.lines.map((line) => <pre><code>{line}</code></pre>)
+              )
+              : (
+                <>
+                  <pre><code>let name = "John Doe";</code></pre>
+                  <pre><code>console.log(name);</code></pre>
+                </>
+              )}
           </div>
           <p class="text-lg">
-            Try to print the text "{code ? code.expectedOutput : "John Doe"}" to the terminal!
+            Try to print the text "{code ? code.expectedOutput : "John Doe"}" to
+            the terminal!
           </p>
         </div>
         <div class="flex flex-col gap-4 sm:w-1/3">
--- sections/Terminal.tsx
+++ sections/Terminal.tsx
@@ -1,2 +1,2 @@
 export { default } from "$store/islands/TerminalHero.tsx";
-export type { Props } from "$store/islands/TerminalHero.tsx"
+export type { Props } from "$store/islands/TerminalHero.tsx";
